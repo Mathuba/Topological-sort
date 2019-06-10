a9d7e180e3fdf52c9eeb144e08782dfb2a2c4ac0
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-10 22:35:51 +0200

    Added efficient algo for topological sort

--- toposort.py
+++ toposort.py
@@ -8,26 +8,23 @@ def add_edge(graph, vertex1, vertex2):
     else:
         graph[vertex1] = vertex2
 
-def dfs(vertex, visited, visited_nodes, graph):
+def dfs(i, vertex, visited, ordering, graph):
     visited[vertex-1] = True
     for neighbour in graph[vertex]:
         if visited[neighbour-1] is False:
-            dfs(neighbour, visited, visited_nodes, graph)
-    visited_nodes.append(vertex)    
+            dfs(i, neighbour, visited, ordering, graph)
+    ordering.insert(0, vertex)
+    return i - 1
     
 def topsort(adj):
     num_of_nodes = len(graph)
     visited = [False] * num_of_nodes
-    ordering = [0] * num_of_nodes
+    ordering = []
     i = num_of_nodes - 1
 
     for vertex in graph:
         if visited[vertex-1] is False:
-            visited_nodes = []
-            dfs(vertex, visited, visited_nodes, graph)
-            for node_id in visited_nodes:
-                ordering[i] = node_id
-                i = i - 1
+            i = dfs(i, vertex, visited, ordering, graph)
     return ordering
 
 if __name__ == '__main__':
